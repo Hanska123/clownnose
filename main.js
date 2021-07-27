faf07cae8d9d8d8da52f24f0dccc8b547b929006
Hanska123 <74179617+Hanska123@users.noreply.github.com>
2021-07-27 14:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+noseX=0;
+noseY=0;
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/TYD59Npm/clone-nose.jpg');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+function modelLoaded(){
+    console.log('posenet is intialized');
+}
+function gotPoses(results){
+    if(results.legnth>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,30,30);
+}
+function take_snapshot(){
+    save('Myfilterimage.png');
+}
